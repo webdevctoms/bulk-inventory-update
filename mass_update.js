5a9c072837a40ec3eace18ea56c6925f0928e600
Michael Peterson <WebDev@ctoms.ca>
2019-10-30 11:27:10 -0600

    added initial promise setup

--- mass_update.js
+++ mass_update.js
@@ -3,32 +3,55 @@
  *@NScriptType ClientScript
  */
 
-define(['N/currentRecord','./k-p.js'],
+define(['N/currentRecord','./k-p.js','./shopify-calls.js'],
 	//not really the entry point function more of a wrapper
-	function(currentRecord,keys){
+	function(currentRecord,keys,shopify){
 		//entry point function
 		function bulkUpdateShopify(){
-            var keyData = keys.getKeys();
-            var rec = currentRecord.get();
+			var rec = currentRecord.get();
             var rec_title = rec.getValue({
                 fieldId:'title'
             });
-			try{
+			var keyData = keys.getKeysU();
+			var options = {
+				keyData:{
+					key:keyData.mesak,
+					pass:keyData.mesap,
+					url:keyData.mesaurl
+				}
+			};
+			return shopify.getAllProducts(options)
+			
+			.then(function(productdata){
+				try{
+					log.debug ({
+						title: 'Title',
+						details: rec_title
+					});
+					
+					log.debug ({
+						title: 'Product Data',
+						details: productdata
+					});
+	
+					return true;
+				}
+				catch(err){
+					log.error({
+						title:err.name,
+						details:err.message
+					});
+					
+					return false;
+				}
+			})
+			
+			.catch(function(err){
 				log.debug ({
-                    title: 'Title',
-                    details: rec_title
+                    title: 'Error getting all product Data',
+                    details: err
                 });
-
-                return true;
-			}
-			catch(err){
-				log.error({
-					title:err.name,
-					details:err.message
-                });
-                
-                return false;
-			}
+			});
 
 		}
 
