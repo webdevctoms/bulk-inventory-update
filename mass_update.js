b1587fd78409679fd8b6f9d9b2e73f53e13e5918
Michael Peterson <WebDev@ctoms.ca>
2019-10-30 14:14:31 -0600

    modified code to make call to restlet, restlet will handle inventory update in background

--- mass_update.js
+++ mass_update.js
@@ -3,61 +3,26 @@
  *@NScriptType ClientScript
  */
 
-define(['N/currentRecord','./k-p.js','./shopify-calls.js'],
+define(['N/currentRecord','N/https','./k-p.js'],
 	//not really the entry point function more of a wrapper
-	function(currentRecord,keys,shopify){
+	function(currentRecord,https,keys){
 		//entry point function
 		function bulkUpdateShopify(){
 			var rec = currentRecord.get();
             var rec_title = rec.getValue({
                 fieldId:'title'
-            });
-			var keyData = keys.getKeysU();
-			var options = {
-				keyData:{
-					key:keyData.mesak,
-					pass:keyData.mesap,
-					url:keyData.mesaurl
-				}
-			};
+			});
+
+			var url = keys.getInventoryUrl().url;
 			if(rec_title.includes('Mass Update Shopify Inventory')){
-				return shopify.getAllProducts(options)
-			
-				.then(function(productdata){
-					try{
-						log.debug ({
-							title: 'Title',
-							details: rec_title
-						});
-						
-						log.debug ({
-							title: 'Product Data',
-							details: productdata
-						});
-		
-						return true;
-					}
-					catch(err){
-						log.error({
-							title:err.name,
-							details:err.message
-						});
-						
-						return false;
-					}
-				})
-				
-				.catch(function(err){
-					log.debug ({
-						title: 'Error getting all product Data',
-						details: err
-					});
+				var dataFromRestlet = https.get(url);
+				log.debug ({
+					title: 'dataFromRestlet Data',
+					details: dataFromRestlet
 				});
-			}
-			else{
 				return true;
 			}
-
+			
 		}
 
 		return{
