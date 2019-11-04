fd24aad7cc8f6ee69d8df944a9c87c7c258f8337
Michael Peterson <WebDev@ctoms.ca>
2019-11-04 10:38:59 -0700

    initial async calls setup

--- mass_update.js
+++ mass_update.js
@@ -13,14 +13,51 @@ define(['N/currentRecord','N/https','./k-p.js'],
                 fieldId:'title'
 			});
 
-			var url = keys.getInventoryUrl().url;
+			//var url = keys.getInventoryUrl().url;
+			var urlData = keys.getTestUrl();
+			var ck = urlData.ck;
+			var url = urlData.url;
+			var tokenUrl = urlData.tokenUrl;
 			if(rec_title.includes('Mass Update Shopify Inventory')){
-				var dataFromRestlet = https.get(url);
-				log.debug ({
-					title: 'dataFromRestlet Data',
-					details: dataFromRestlet
-				});
-				return true;
+				var header = {
+					Authorization:ck
+				}
+				return https.get.promise({
+					url:tokenUrl,
+					headers:header
+				}).then(function(tokenData){
+					/*
+					log.debug ({
+						title: 'token Data',
+						details: tokenData
+					});
+					*/
+					//pass token in this request
+					return https.get.promise({
+						url:url,
+						headers:header
+					})
+				})
+
+				.then(function(inventoryData){
+					/*
+					log.debug ({
+						title: 'inventory Data',
+						details: inventoryData
+					});
+					*/
+					return true;
+				})
+
+				.catch(function(err){
+					log.debug ({
+						title: 'Error Getting data',
+						details: err
+					});
+
+					return true;
+				});	
+				
 			}
 			else{
 				return true;
